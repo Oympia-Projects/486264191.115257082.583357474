5b11764a6c7db96ba0e8267f949c58cead221403
Taylor <78822889+TaylorA1dan@users.noreply.github.com>
2022-07-27 03:20:24 -0400

    Update reboot.js

--- prefix/reboot.js
+++ prefix/reboot.js
@@ -39,13 +39,10 @@ const filter = i => i.customId === 'sonic';
 const collector = message.channel.createMessageComponentCollector({ filter, time: 7000, max: 1 });
 
 collector.on('collect', async i => {
-	await i.reply({ embeds: [rEmbed2] }).then(msg => {
-		setTimeout(function(){
-			chan2.send({ content: 'Bot is Online ~ Everything is Functional'})
-	}, 10000);
-	  })
+	await i.reply({ embeds: [rEmbed2] })
 	  .then(client.destroy())
 	  .then(client.login(process.env.bot_token))
+      .then(client.user.setStatus('dnd'));
 
 });
 
