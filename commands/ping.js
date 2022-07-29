f207645d5bd73eba21ea4a4b63c2e71909261b52
Taylor <78822889+TaylorA1dan@users.noreply.github.com>
2022-07-29 08:12:31 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,13 +4,22 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription(`Roll's a random number between 1-100.`),
+		.setDescription(`Pong`),
 	async execute(interaction, message, client) {
 
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#00FFFF')
-            .setDescription(` **:ping_pong:Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms.:ping_pong:**`)
-        interaction.reply({embeds: [exampleEmbed]})
+        const newPingEmbed = new Discord.MessageEmbed()
+        
+        //.setTitle(`:stopwatch: The Ping Meter`) of choice
+        .addFields(
+            {name: `:robot: The Bot's Latency`, value: `**${Date.now() - message.createdTimestamp}**ms.`},
+            {name: `:desktop: The API's Latency`, value: `**${Math.round(client.ws.ping)}**ms.`}
+        )
+        .setColor('RANDOM')
+        .setFooter(`Requested by ${interaction.user}`, avatar)
+        .setTimestamp()
+    
+
+        interaction.reply({embeds: [newPingEmbed]})
     }
         
 }
